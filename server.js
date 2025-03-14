738b0c33120a08665325e733051b0e76e1cfee69
Shinbad10 <bumble6320@gmail.com>
2025-03-14 10:40:32 +0700

    Update API

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 import express from "express";
-import http from "http";
 import cors from "cors";
 import authRoutes from "./src/routes/authRoutes.js";
 import appRoutes from "./src/routes/index.js";
 import { setupSocket } from "./src/websocket/socket.js";
-import config from './src/config/dotenv.js'
+import config from './src/config/dotenv.js';
+
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-  }));
-  
+app.use(cors({ origin: '*' }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", appRoutes);
 
-setupSocket(server);
-
-server.listen(config.PORT, () => console.log(`Server running on port ${config.PORT}`));
+// Export để Vercel sử dụng như một API handler
+export default app;
